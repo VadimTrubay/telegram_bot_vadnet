31b9345f4622a18798576c8894d97ee1c6d72c6f
VadimTrubay <vadnetvadnet@ukr.net>
2024-05-19 21:10:40 +0300

    create options file

--- index.js
+++ index.js
@@ -1,29 +1,12 @@
 import TelegramBot from "node-telegram-bot-api";
+import {gameOptions, againOptions} from "./options.js";
+
 
 const token = '7058894670:AAEk1J5y51h3H_2CxOoqbsertF8AhbSQ9f0'
 
 const bot = new TelegramBot(token, {polling: true});
 
 let chats = {};
-
-const gameOptions = {
-  reply_markup: JSON.stringify({
-    inline_keyboard: [
-      [{'text': '1', callback_data: '1'}, {'text': '2', callback_data: '2'}, {'text': '3', callback_data: '3'}],
-      [{'text': '4', callback_data: '4'}, {'text': '5', callback_data: '5'}, {'text': '6', callback_data: '6'}],
-      [{'text': '7', callback_data: '7'}, {'text': '8', callback_data: '8'}, {'text': '9', callback_data: '9'}],
-      [{'text': '0', callback_data: '0'}],
-    ]
-  })
-}
-
-const againOptions = {
-  reply_markup: JSON.stringify({
-    inline_keyboard: [
-      [{'text': 'Game again?', callback_data: '/again'}],
-    ]
-  })
-}
 
 const startGame = async (chatId) => {
   chats['chatId'] = Math.floor(Math.random() * 10).toString();
